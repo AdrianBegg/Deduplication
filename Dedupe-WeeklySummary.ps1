b61799514e2eed59d7b17a24da8f3e6117fcb89b
Adrian Begg <jimmydea@gmail.com>
2017-01-10 21:37:23 +1000

    Some minor changes
    Added UnoptimisedFileCount monitoring and some cosmetic changes

--- Dedupe-WeeklySummary.ps1
+++ Dedupe-WeeklySummary.ps1
@@ -21,14 +21,19 @@
 [string] $MailServerPassword = "<INSERT PASSWORD>"
 [int] $daysSinceLastJobAlert = 9 # Number of days to alert if one of the jobs has not run
 [int] $freeSpaceGBAlert = 30 # Free space in GB to alert if the Free Space falls below
+[int] $intUnoptimisedFileAlert = 30 # The number of files that are not currently deduplicated that are eligble (may indicate an issue with the jobs)
 
 # MAIN
 Import-Module Deduplication
+[string] $strSuccessMessage = "The operation completed successfully."
 $secpasswd = ConvertTo-SecureString $MailServerPassword -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ($MailServerUserName, $secpasswd)
 
 $dedupeStatus = Get-DedupStatus
 [string] $summary = ""
+
+# Calculate the number of Inpolicy files that are not yet deduped
+[int] $unoptimizedFiles = $dedupeStatus.InPolicyFilesCount - $dedupeStatus.OptimizedFilesCount
 
 # Check if the Deduplication jobs have all run in the last 9 days; if they have not thrown an alert or if the last result was anything other than successful
 Foreach($dedupeObject in $dedupeStatus){
@@ -43,14 +48,17 @@ Foreach($dedupeObject in $dedupeStatus){
 	If($dedupeObject.LastScrubbingTime -lt $dtDaysLastThreshold){
 		$summary += ":: ALERT :: Scrubbing has not run in the last $daysSinceLastJobAlert days for $strVolume ! Investigate.`r`n"
 	}
-	If(!($dedupeObject.LastOptimizationResultMessage.StartsWith("The operation completed successfully."))){
+	If(!($dedupeObject.LastOptimizationResultMessage.StartsWith($strSuccessMessage))){
 		$summary += ":: ALERT :: The last Optimization job did not complete successfully for $strVolume ! Investigate.`r`n"
 	}
-	If(!($dedupeObject.LastGarbageCollectionResultMessage.StartsWith("The operation completed successfully."))){
+	If(!($dedupeObject.LastGarbageCollectionResultMessage.StartsWith($strSuccessMessage))){
 		$summary += ":: ALERT :: The last Garbage Collection job did not complete successfully for $strVolume ! Investigate.`r`n"
 	}
-	If(!($dedupeObject.LastScrubbingResultMessage.StartsWith("The operation completed successfully."))){
+	If(!($dedupeObject.LastScrubbingResultMessage.StartsWith($strSuccessMessage))){
 		$summary += ":: ALERT :: The last Scrubbing job did not complete successfully for $strVolume ! Investigate.`r`n"
+	}
+	if($unoptimizedFiles -gt $intUnoptimisedFileAlert){
+		$summary += ":: ALERT :: The unoptimized file count for $strVolume is $unoptimizedFiles ! Investigate.`r`n"
 	}
 	# Finally check Free Space
 	If(($dedupeObject.FreeSpace / 1GB) -lt $freeSpaceGBAlert){
